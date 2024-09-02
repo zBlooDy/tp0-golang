87faafa753b823528b91005bd443a0f3d9bbbc7b
Thiago <tperezgiacchetta@frba.utn.edu.ar>
2024-09-02 02:07:35 +0000

    Etapa 3

--- server/main.go
+++ server/main.go
@@ -11,7 +11,6 @@ func main() {
 	mux.HandleFunc("/paquetes", utils.RecibirPaquetes)
 	mux.HandleFunc("/mensaje", utils.RecibirMensaje)
 
-	panic("no implementado!")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
